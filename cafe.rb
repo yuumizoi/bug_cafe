dff45a2abee8ac723c95362871b9783a311693bc
yuumi.mizoi <yuumi.mizoi@gmail.com>
2024-12-06 19:27:45 +0900

    変数名をリファクタリングしました

--- cafe.rb
+++ cafe.rb
@@ -25,9 +25,9 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink_order = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
-total = ((DRINKS[order1][:price]).to_i) + ((FOODS[order2][:price]).to_i)
+food_order = take_order(FOODS)
+total = ((DRINKS[drink_order][:price]).to_i) + ((FOODS[food_order][:price]).to_i)
 puts "お会計は#{total}円になります。ありがとうございました！"
